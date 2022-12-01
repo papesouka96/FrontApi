17e0b8345f6556239662e5f2df2a129d57e0ce4a
gonarhane <gonar90@gmail.com>
2022-12-01 16:54:10 +0000

    creation de ma branche

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private httpClient : HttpClient) { }  /* injection des dépendances */
+}
